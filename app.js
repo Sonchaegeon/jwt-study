37705449c4683f472b78f3285a3ad209a1d532b1
Chaegeon <thscorjs1002@naver.com>
2020-09-01 13:26:23 +0900

    Connect db

--- app.js
+++ app.js
@@ -1,5 +1,16 @@
 var express = require('express');
 var app = express();
+var dotenv = require('dotenv');
+var mongoose = require('mongoose');
+
+dotenv.config();
+
+mongoose.connect(
+    process.env.DB_CONNECT,
+    { useNewUrlParser: true},
+    () => console.log('connected to db!')
+);
+
 var authRoute = require('./routes/auth');
 
 app.use('/api/user', authRoute);
