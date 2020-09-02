6e0772a22e84d88696a1d2380a7c5f65b2a74997
Chaegeon <thscorjs1002@naver.com>
2020-09-02 12:07:56 +0900

    Authorization

--- app.js
+++ app.js
@@ -1,19 +1,44 @@
-var express = require('express');
-var app = express();
-var dotenv = require('dotenv');
-var mongoose = require('mongoose');
+require('dotenv').config()
 
-dotenv.config();
+var express = require('express')
+var app = express()
 
-mongoose.connect(
-    process.env.DB_CONNECT,
-    { useNewUrlParser: true},
-    () => console.log('connected to db!')
-);
+var jwt = require('jsonwebtoken')
 
-var authRoute = require('./routes/auth');
+app.use(express.json())
 
-app.use('/api/user', authRoute);
+var posts = [
+    {
+        username: 'Son',
+        title: 'Post 1'
+    }, {
+        username: 'Howon',
+        title: 'Post 2'
+    }
+]
 
+app.get('/posts', authenticateToken, (req, res) => {
+    res.json(posts.filter(post => post.username === req.user.name))
+})
 
-app.listen(3000, () => console.log('Server running : 3000'));
+app.post('/login', (req, res) => {
+    const username = req.body.username
+    const user = { name: username }
+
+    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+    res.json({accessToken: accessToken })
+})
+
+function authenticateToken(req, res, next){
+    const authHeader = req.headers['authorization']
+    const token = authHeader && authHeader.split(' ')[1]
+    if(token == null) return res.sendStatus(401)
+
+    jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
+        if(err) throw err
+        req.user = user
+        next()
+    })
+}
+
+app.listen(3000)
--- routes/auth.js
+++
@@ -1,7 +0,0 @@
-var router = require('express').Router();
-
-router.post('/register', (req, res) => {
-    res.send('Register');
-});
-
-module.exports = router;
