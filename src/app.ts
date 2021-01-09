37e61d260c1f15b79263c1461427f9f026a7fcde
sonchaegeon <thscorjs1002@naver.com>
2021-01-09 19:53:04 +0900

    UPDATE; settings, routes, middlewares
    set port, auth routes, morgan add

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,17 @@
 import express, { Application } from "express";
+import morgan from "morgan";
 
 const app: Application = express();
+
+import authRoutes from "./routes/auth";
+
+// Settings
+app.set('port', 4000);
+
+// Routes
+app.use(authRoutes);
+
+// Middlewares
+app.use(morgan('dev'));
 
 export default app;
