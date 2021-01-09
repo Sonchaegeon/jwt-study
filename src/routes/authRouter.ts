d480170f6fb5b05baf6ce7dff138db91b4ca4d38
sonchaegeon <thscorjs1002@naver.com>
2021-01-09 23:30:02 +0900

    UPDATE; profile api router
    auth router

--- src/routes/authRouter.ts
+++ src/routes/authRouter.ts
@@ -1,10 +1,13 @@
 import { Router } from "express";
 const router: Router = Router();
 
+import {TokenValidation} from "../middlewares/verifyToken";
+
 import { signup, signin, profile } from '../controllers/authController';
 
 router.post('/signup', signup);
 router.post('/signin', signin);
-router.get('/profile', profile);
+
+router.get('/profile', TokenValidation, profile);
 
 export default router;
