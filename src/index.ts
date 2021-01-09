04cbfbd52386aaacc277074952e8a721a76f5264
sonchaegeon <thscorjs1002@naver.com>
2021-01-09 19:53:29 +0900

    UPDATE; init function

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
 import app from "./app";
 
-app.listen(3000);
-console.log('Server running on port 3000');
+function init() {
+    app.listen(app.get('port'));
+    console.log('Server running on port', app.get('port'));
+}
+
+init();
